ecd403a813cde30c0db813d8034e336a5a2bf3b3
shaik fazil basha <fazilshaik103@gmail.com>
2024-10-08 10:00:12 +0530

    updated mongodb.ts file

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -5,10 +5,10 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI
-const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-}
+// const options = {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+// }
 
 let client: MongoClient
 let clientPromise: Promise<MongoClient>
@@ -16,18 +16,19 @@ let clientPromise: Promise<MongoClient>
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  // eslint-disable-next-line prefer-const
   let globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>
   }
 
   if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options)
+    client = new MongoClient(uri)
     globalWithMongo._mongoClientPromise = client.connect()
   }
   clientPromise = globalWithMongo._mongoClientPromise
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options)
+  client = new MongoClient(uri)
   clientPromise = client.connect()
 }
 
