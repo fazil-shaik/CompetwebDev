6c0d6a2bde499db4385ef8365f8ea4a48363645a
shaik fazil basha <fazilshaik103@gmail.com>
2024-10-08 10:30:08 +0530

    made changes in mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,32 +1,40 @@
-/* eslint-disable prefer-const */
-import { MongoClient } from 'mongodb'
+import { MongoClient, MongoClientOptions } from 'mongodb'
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local')
 }
 
 const uri = process.env.MONGODB_URI
-const options = {}
+const options: MongoClientOptions = {}
 
-let client
+let client: MongoClient
 let clientPromise: Promise<MongoClient>
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined
+}
 
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  let globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise?: Promise<MongoClient>
-  }
-
-  if (!globalWithMongo._mongoClientPromise) {
+  if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options)
-    globalWithMongo._mongoClientPromise = client.connect()
+    global._mongoClientPromise = client.connect()
+      .catch(err => {
+        console.error('Failed to connect to MongoDB', err)
+        throw err
+      })
   }
-  clientPromise = globalWithMongo._mongoClientPromise
+  clientPromise = global._mongoClientPromise
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options)
   clientPromise = client.connect()
+    .catch(err => {
+      console.error('Failed to connect to MongoDB', err)
+      throw err
+    })
 }
 
 // Export a module-scoped MongoClient promise. By doing this in a
