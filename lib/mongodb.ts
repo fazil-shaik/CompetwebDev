48abcbb140d2112e9089cde14a09029fff511ee3
shaik fazil basha <fazilshaik103@gmail.com>
2024-10-08 10:41:53 +0530

    mongodb changes

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -7,11 +7,6 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI
 const options: MongoClientOptions = {}
 
-// Validate the MongoDB URI
-if (!uri.startsWith('mongodb://') && !uri.startsWith('mongodb+srv://')) {
-  throw new Error('Invalid MONGODB_URI. It should start with mongodb:// or mongodb+srv://')
-}
-
 let client: MongoClient
 let clientPromise: Promise<MongoClient>
 
@@ -21,6 +16,8 @@ declare global {
 }
 
 if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options)
     global._mongoClientPromise = client.connect()
@@ -31,6 +28,7 @@ if (process.env.NODE_ENV === 'development') {
   }
   clientPromise = global._mongoClientPromise
 } else {
+  // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options)
   clientPromise = client.connect()
     .catch(err => {
@@ -39,4 +37,6 @@ if (process.env.NODE_ENV === 'development') {
     })
 }
 
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
 export default clientPromise
